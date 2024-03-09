5a5721b715082dfb461117d90fa8951bac9a68ad
GrynnRadovid <kxisrevolution@gmail.com>
2024-03-09 21:21:17 +0200

    Updated config_file path to use abs path

--- modules/commons.py
+++ modules/commons.py
@@ -1,11 +1,16 @@
 import json
-
-def get_data_from_config(data, config_file='config.json'):
+import os
+def get_data_from_config(data):
     """
     Reads the city name from a configuration file.
-    :param config_file: The path to the configuration.json file
+    :param data: The data that you want to read from the config.json file
     :return: The city name read from the file.
     """
+    # Get abs path for config.json
+    config_file = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+        'config.json'
+    )
     with open(config_file, 'r') as file:
         config_dict = json.load(file)
         return config_dict[data]
